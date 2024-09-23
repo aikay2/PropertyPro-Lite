b71bb396490ff70694aad8cfc818e92feddfc63d
Ikechukwu Anyikwa <austineanyikwa@gmail.com>
2024-09-23 15:06:11 +0100

    modified CustomUser model

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,59 @@
+# admin.py in your authentication app
 from django.contrib import admin
-from .models import CustomUser
+from django import forms
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Agent, Customer
 
-# Register your models here.
-admin.site.register(CustomUser)
+class CustomUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+    user_type = forms.ChoiceField(choices=[
+        ('agent', 'Agent'),
+        ('customer', 'Customer')
+    ])
+    
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'first_name', 'last_name', 'address', 'phoneNumber', 'user_type')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data["password1"]
+        password2 = self.cleaned_data["password2"]
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords do not match.")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+
+        if commit:
+            user.save()
+            user_type = self.cleaned_data['user_type']
+            if user_type == 'agent':
+                Agent.objects.create(user=user)
+            else:
+                Customer.objects.create(user=user)
+        
+        return user
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    model = CustomUser
+    list_display = ('username', 'first_name', 'last_name', 'is_staff')
+
+    # Specify fields for the admin
+    fieldsets = (
+        (None, {'fields': ('username', 'first_name', 'last_name', 'address', 'phoneNumber', 'is_active', 'is_staff')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'first_name', 'last_name', 'address', 'phoneNumber', 'password1', 'password2', 'user_type'),
+        }),
+    )
+    
+admin.site.register(CustomUser, CustomUserAdmin)
--- authentication/models.py
+++ authentication/models.py
@@ -4,10 +4,16 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class CustomUser(AbstractUser):
+    USER_TYPE_CHOICES = (
+        ('agent', 'Agent'),
+        ('customer', 'Customer'),
+    )
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     address = models.TextField(max_length=75)
     phoneNumber = PhoneNumberField(null=False, blank=False, unique=False)
+    created_on = models.DateTimeField(auto_now_add=True)
     
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email', 'first_name', 'last_name', 'address', 'phoneNumber']
